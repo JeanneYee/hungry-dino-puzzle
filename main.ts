9dfe38fb51612dadfef78b5fa25c4e5be1391fa4
JeanneYee <waiying.yee@gmail.com>
2020-05-24 20:40:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,325 @@
+namespace SpriteKind {
+    export const PlayerB = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+        d . . . . . . . . . . . . . . d
+        . f f f . . . . . . . . . . . .
+        . f 9 9 f f . . . . . . . . . .
+        . . f 9 9 9 f f . . . . . . . .
+        . . f 9 8 8 9 9 f f . . . . . .
+        . . . f 9 8 8 8 9 9 f f . . . .
+        . . . f 9 8 8 8 8 8 9 9 f f . .
+        . . . . f 9 8 8 8 8 8 8 9 9 f .
+        . . . . f 9 8 8 8 8 8 8 9 9 f .
+        . . . f 9 8 8 8 8 8 9 9 f f . .
+        . . . f 9 8 8 8 9 9 f f . . . .
+        . . f 9 8 8 9 9 f f . . . . . .
+        . . f 9 9 9 f f . . . . . . . .
+        . f 9 9 f f . . . . . . . . . .
+        . f f f . . . . . . . . . . . .
+        d . . . . . . . . . . . . . . d
+    `
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+        d d d d d d d d d d d d d d d d
+    `
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+        d . . . . . . . . . . . . . . d
+        . f f . . . . . . . . . . f f .
+        . f 9 f f . . . . . . f f 9 f .
+        . f 9 9 9 f f . . f f 9 9 9 f .
+        . . f 9 8 9 9 f f 9 9 8 9 f . .
+        . . f 9 8 8 8 9 9 8 8 8 9 f . .
+        . . . f 9 8 8 8 8 8 8 9 f . . .
+        . . . f 9 8 8 8 8 8 8 9 f . . .
+        . . . . f 9 8 8 8 8 9 f . . . .
+        . . . . f 9 8 8 8 8 9 f . . . .
+        . . . . . f 9 8 8 9 f . . . . .
+        . . . . . f 9 8 8 9 f . . . . .
+        . . . . . . f 9 9 f . . . . . .
+        . . . . . . f 9 9 f . . . . . .
+        . . . . . . . f f . . . . . . .
+        d . . . . . . . . . . . . . . d
+    `
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+        d . . . . . . . . . . . . . . d
+        . . . . . . . f f . . . . . . .
+        . . . . . . f 9 9 f . . . . . .
+        . . . . . . f 9 9 f . . . . . .
+        . . . . . f 9 8 8 9 f . . . . .
+        . . . . . f 9 8 8 9 f . . . . .
+        . . . . f 9 8 8 8 8 9 f . . . .
+        . . . . f 9 8 8 8 8 9 f . . . .
+        . . . f 9 8 8 8 8 8 8 9 f . . .
+        . . . f 9 8 8 8 8 8 8 9 f . . .
+        . . f 9 8 8 8 9 9 8 8 8 9 f . .
+        . . f 9 8 9 9 f f 9 9 8 9 f . .
+        . f 9 9 9 f f . . f f 9 9 9 f .
+        . f 9 f f . . . . . . f f 9 f .
+        . f f . . . . . . . . . . f f .
+        d . . . . . . . . . . . . . . d
+    `
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+        d . . . . . . . . . . . . . . d
+        . . . . . . . . . . . . f f f .
+        . . . . . . . . . . f f 9 9 f .
+        . . . . . . . . f f 9 9 9 f . .
+        . . . . . . f f 9 9 8 8 9 f . .
+        . . . . f f 9 9 8 8 8 9 f . . .
+        . . f f 9 9 8 8 8 8 8 9 f . . .
+        . f 9 9 8 8 8 8 8 8 9 f . . . .
+        . f 9 9 8 8 8 8 8 8 9 f . . . .
+        . . f f 9 9 8 8 8 8 8 9 f . . .
+        . . . . f f 9 9 8 8 8 9 f . . .
+        . . . . . . f f 9 9 8 8 9 f . .
+        . . . . . . . . f f 9 9 9 f . .
+        . . . . . . . . . . f f 9 9 f .
+        . . . . . . . . . . . . f f f .
+        d . . . . . . . . . . . . . . d
+    `
+}
+function move_right (mySprite: Sprite) {
+    mySprite.setVelocity(30, 0)
+}
+function turn_selected_arrow (num: number) {
+    tiles.placeOnTile(arrow_selection, arrow_locationn_list[num])
+    if (arrow_selection.tileKindAt(TileDirection.Center, myTiles.tile1)) {
+        tiles.setTileAt(arrow_locationn_list[num], myTiles.tile3)
+        if (tilemap.tileIsWall(tilemap.locationInDirection(arrow_locationn_list[num], CollisionDirection.Bottom))) {
+            turn_selected_arrow(num)
+        }
+    } else if (arrow_selection.tileKindAt(TileDirection.Center, myTiles.tile3)) {
+        tiles.setTileAt(arrow_locationn_list[num], myTiles.tile5)
+        if (tilemap.tileIsWall(tilemap.locationInDirection(arrow_locationn_list[num], CollisionDirection.Left))) {
+            turn_selected_arrow(num)
+        }
+    } else if (arrow_selection.tileKindAt(TileDirection.Center, myTiles.tile5)) {
+        tiles.setTileAt(arrow_locationn_list[num], myTiles.tile4)
+        if (tilemap.tileIsWall(tilemap.locationInDirection(arrow_locationn_list[num], CollisionDirection.Top))) {
+            turn_selected_arrow(num)
+        }
+    } else {
+        tiles.setTileAt(arrow_locationn_list[num], myTiles.tile1)
+        if (tilemap.tileIsWall(tilemap.locationInDirection(arrow_locationn_list[num], CollisionDirection.Right))) {
+            turn_selected_arrow(num)
+        }
+    }
+}
+function SetupGame () {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`0a000800070a0a0a0a0a0a0e0e0e0a0e0e0a0e0e0a0a0a0a0a0e0e0a0e0e0a0e0e0a0a0e0e0a0e0e0a0e0e0a0a0a0a0a0a0a0c0a0a0a0a0e0e0e0a0e0a0e0e0a0a0a0a0a0d0e0a0a0a0d0e0e0e0e0e0e0e0e0e0e`,
+            img`
+                . . . . . . . 2 2 2
+                . 2 2 . 2 2 . . . .
+                . 2 2 . 2 2 . 2 2 .
+                . 2 2 . 2 2 . 2 2 .
+                . . . . . . . . . .
+                . 2 2 2 . 2 . 2 2 .
+                . . . . . 2 . . . .
+                2 2 2 2 2 2 2 2 2 2
+            `,
+            [myTiles.tile0,sprites.castle.tilePath5,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.floorLight1,sprites.dungeon.floorLight0,sprites.builtin.brick,myTiles.tile1,sprites.builtin.field0,sprites.builtin.field1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,sprites.dungeon.floorDark0],
+            TileScale.Sixteen
+        ))
+    scene.setBackgroundColor(13)
+    dino = sprites.create(img`
+        d . . . . . . . . . . . . . . d
+        . . . . . . f f f f f f f f . .
+        . . . . 4 f 7 7 7 7 7 7 7 7 f .
+        . . . . 4 f 7 7 7 1 f 1 7 7 f .
+        . . . . f f 7 7 7 1 f 1 7 7 f .
+        . . . . 4 f 7 7 7 1 1 1 7 7 f .
+        . . . 4 4 f 7 7 f 7 7 7 7 7 f .
+        . . . f f 7 7 7 f f f f f f . .
+        . . . 4 f 7 7 7 7 7 7 f . . . .
+        . . 4 4 f 7 f 7 7 7 7 7 7 f . .
+        . . f f 7 7 f 7 7 7 7 7 7 f . .
+        . 4 4 f 7 7 7 7 d d 7 f . . . .
+        4 f f 7 7 7 7 d d d 7 f . . . .
+        f 7 7 7 7 7 7 d d d 7 f . . . .
+        f f f f f 7 7 f f d 7 f . . . .
+        d . . . f f f . . f f f . . . d
+    `, SpriteKind.Player)
+    tiles.placeOnRandomTile(dino, myTiles.tile2)
+    move_randomly(dino)
+    angry_dragon = sprites.create(img`
+        d . . . . . . . . . . . . d . d
+        . e e . . . . e e e . . . . . .
+        . d d c . . c d d c . . . . . .
+        . b d d f f d d b c . . . . . .
+        . 3 b d d b d b 3 c . . . . . .
+        . b 3 d d d d 3 b f . . . . . .
+        . d d d d d d d d e . . . . . .
+        . d f d d d d f d e b f b . . .
+        . d d f d d f d d f f d f . . .
+        . b d d b b d d 2 b f d f . . .
+        . f 2 2 2 2 2 2 d b d b f . . .
+        . f d d d d d d d f f f . . . .
+        . f d b d f f f d f . . . . . .
+        d . . . . . . . . . . . . d . .
+        . . . . . . . . . . . . . . . .
+        d . . . . . . . . . . . . . . d
+    `, SpriteKind.Player)
+    tiles.placeOnRandomTile(angry_dragon, myTiles.tile2)
+    move_randomly(angry_dragon)
+    worm = sprites.create(img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . f f f . . . . . . . . .
+        . . . f 5 5 5 f . . . . . . . .
+        . . f 5 5 5 5 5 f . . . . . . .
+        . . f 5 f 5 f 5 f . . . . . . .
+        . . f 5 5 5 5 5 f . . . . . . .
+        . . f 5 5 5 5 5 f . . . . . . .
+        . . . f 5 f 5 f . . . . . . . .
+        . . . . f 5 5 f . . . . . . . .
+        . . . . f 5 f . . . . . . . . .
+        . . . f 5 5 f . . f f f . . . .
+        . . . f 5 5 f f f 5 5 5 f . . .
+        . . . f 5 5 5 5 5 5 f 5 5 f . .
+        . . . . f f f f f . . f f . . .
+        . . . . . . . . . . . . . . . .
+    `, SpriteKind.Food)
+    tiles.placeOnRandomTile(worm, myTiles.tile2)
+    arrow_selection = sprites.create(img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `, SpriteKind.Projectile)
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    turn_selected_arrow(0)
+})
+function move_upwards (mySprite: Sprite) {
+    mySprite.setVelocity(0, -30)
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    turn_selected_arrow(1)
+})
+function move_randomly (mySprite: Sprite) {
+    random_direction = Math.randomRange(1, 4)
+    if (random_direction == 1) {
+        move_left(mySprite)
+    } else if (random_direction == 2) {
+        move_right(mySprite)
+    } else if (random_direction == 3) {
+        move_upwards(mySprite)
+    } else {
+        move_downwards(mySprite)
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    tiles.placeOnRandomTile(worm, myTiles.tile2)
+})
+function move_left (mySprite: Sprite) {
+    mySprite.setVelocity(-30, 0)
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    turn_selected_arrow(2)
+})
+function move_downwards (mySprite: Sprite) {
+    mySprite.setVelocity(0, 30)
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    turn_selected_arrow(3)
+})
+let random_direction = 0
+let worm: Sprite = null
+let angry_dragon: Sprite = null
+let dino: Sprite = null
+let arrow_selection: Sprite = null
+let arrow_locationn_list: tiles.Location[] = []
+SetupGame()
+info.setScore(0)
+for (let value of tiles.getTilesByType(myTiles.tile1)) {
+    arrow_locationn_list.push(value)
+}
+for (let value of tiles.getTilesByType(myTiles.tile3)) {
+    arrow_locationn_list.push(value)
+}
+for (let value of tiles.getTilesByType(myTiles.tile5)) {
+    arrow_locationn_list.push(value)
+}
+for (let value of tiles.getTilesByType(myTiles.tile4)) {
+    arrow_locationn_list.push(value)
+}
+game.onUpdate(function () {
+    for (let value of sprites.allOfKind(SpriteKind.Player)) {
+        if (value.vx == 0 && value.vy == 0) {
+            move_randomly(value)
+        }
+        for (let value2 of tiles.getTilesByType(myTiles.tile5)) {
+            if (tilemap.locationXY(value2, tilemap.XY.x) == value.x && tilemap.locationXY(value2, tilemap.XY.y) == value.y) {
+                move_left(value)
+            }
+        }
+        for (let value2 of tiles.getTilesByType(myTiles.tile1)) {
+            if (tilemap.locationXY(value2, tilemap.XY.x) == value.x && tilemap.locationXY(value2, tilemap.XY.y) == value.y) {
+                move_right(value)
+            }
+        }
+        for (let value2 of tiles.getTilesByType(myTiles.tile4)) {
+            if (tilemap.locationXY(value2, tilemap.XY.x) == value.x && tilemap.locationXY(value2, tilemap.XY.y) == value.y) {
+                move_upwards(value)
+            }
+        }
+        for (let value2 of tiles.getTilesByType(myTiles.tile3)) {
+            if (tilemap.locationXY(value2, tilemap.XY.x) == value.x && tilemap.locationXY(value2, tilemap.XY.y) == value.y) {
+                move_downwards(value)
+            }
+        }
+    }
+})
